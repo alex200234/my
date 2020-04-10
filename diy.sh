86b29e459783d942dfdc150338b43315f4b59c3c
alex200234 <62095851+alex200234@users.noreply.github.com>
2020-04-11 02:51:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+mv -f mt7621.mk openwrt/target/linux/ramips/image
+mv -f mt7621_phicomm_k2p.dts openwrt/target/linux/ramips/dts
