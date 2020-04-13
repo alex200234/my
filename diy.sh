9edd69787533a3a2ff1b75efa8ec914847dc7f85
alex200234 <62095851+alex200234@users.noreply.github.com>
2020-04-13 16:57:14 +0800

    Create diy.sh

---
+++ diy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+#mv -vf ../mt7621.mk ./target/linux/ramips/image/mt7621.mk
